387159211cbd570a0b6abd218d4bb736a7613200
keleris32 <charles.ejiegbu@gmail.com>
2022-06-13 19:50:00 +0100

    feat: more changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import AppRoutes from './routes/Routes';
+import { ToastContainer } from 'react-toastify';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import 'react-loading-skeleton/dist/skeleton.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={6000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        draggable={false}
+        closeOnClick
+        pauseOnHover
+      />
+      <AppRoutes />;
+    </>
+  );
 }
 
 export default App;
