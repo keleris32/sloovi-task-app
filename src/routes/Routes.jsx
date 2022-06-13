e55c2abf8fcd2d85873736adb89fc9fc9b232135
keleris32 <charles.ejiegbu@gmail.com>
2022-06-13 19:46:01 +0100

    feat: modified routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 
 import { Home, Login, NotFoundRoute } from '../pages';
+import { Layout, RequireAuth } from '../components';
 
 function AppRoutes() {
+  const location = useLocation();
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFoundRoute />} />
+    <AnimatePresence exitBeforeEnter>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Layout />}>
+          {/* Public routes */}
+          <Route path="login" element={<Login />} />
+
+          {/* Private routes */}
+          <Route element={<RequireAuth />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+
+          {/* catch all */}
+          <Route path="*" element={<NotFoundRoute />} />
+        </Route>
       </Routes>
-    </BrowserRouter>
+    </AnimatePresence>
   );
 }
 
